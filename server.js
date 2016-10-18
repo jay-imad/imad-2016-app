46f16d9ff9d75a73237c7232be3ffdc0a015de9e
jay-imad <jayendrangs821@gmail.com>
2016-10-18 15:53:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ app.get('/article-one', function (req, res){
 });
 
 app.get('/article-two', function (req, res){
-    res('article two will be served here');
+    res.send('article two will be served here');
 });
 
 app.get('/article-three', function (req, res){
-    res('article three will be served here');
+    res.send('article three will be served here');
 });
 
 app.get('/ui/style.css', function (req, res) {
